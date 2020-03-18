6f35ec90d9fca1b02d0ca299220e6c7ca753a1fa
Albert Suraliński <albert.suralinski@gmail.com>
2020-03-18 23:22:29 +0100

    Added generic form to get extracting website task.

--- website_content_extractor/views.py
+++ website_content_extractor/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import FormView
 
-# Create your views here.
+from website_content_extractor.forms import QueueTaskForm
+
+
+class QueueTaskView(FormView):
+    template_name = 'queue_task_form.html'
+    form_class = QueueTaskForm
+    success_url = '/'
