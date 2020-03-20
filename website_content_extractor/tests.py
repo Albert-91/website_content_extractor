7edffa1605065e910ba6d06bd0666b1423783428
Albert Suraliński <albert.suralinski@gmail.com>
2020-03-20 19:00:30 +0100

    Added function getting all images urls from website.

--- website_content_extractor/tests.py
+++ website_content_extractor/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+import requests
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from requests.exceptions import MissingSchema
+from website_content_extractor.utils import get_url_photos_from_website
+
+
+# ValidationError, MissingSchema, requests.HTTPError
+
+# print(get_url_photos_from_website('www.wwww.pl/'))
+
+
+class GetUrlPhotosFromWebsite(TestCase):
+
+    def test__photo_url_extractor__should__return__None_if_url_has_no_scheme(self):
+        self.assertEqual(get_url_photos_from_website('www.google.pl'), None)
+
+    def test__photo_url_extractor__should__return__None_if_domain_does_not_exist(self):
+        self.assertEqual(get_url_photos_from_website('http://www.onetwothree.pl'), None)
+
+    def test__photo_url_extractor__should__return__None_input_is_not_correct_url(self):
+        self.assertEqual(get_url_photos_from_website('http://www.onetwothreepl'), None)
+
+    def test__photo_url_extractor__should__return__not_empty_list(self):
+        len_of_list = len(get_url_photos_from_website('http://www.google.pl'))
+        self.assertGreater(len_of_list, 0)
+
+    def test__photo_url_extractor__should__return__list_with_correct_urls(self):
+        photo_urls = get_url_photos_from_website('https://en.wikipedia.org/wiki/Hulk')
+        for url in photo_urls:
+            self.assertNotEqual(get_url_photos_from_website(url), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ website_content_extractor/utils.py
@@ -0,0 +1,49 @@
+import logging
+from typing import Text, List, Union
+from urllib.parse import urlparse
+
+import bs4 as bs
+import requests
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+from requests import Response
+from requests.exceptions import MissingSchema, ConnectionError
+
+logger = logging.getLogger(__name__)
+
+
+def get_url_photos_from_website(url: Text) -> Union[List[Text], None]:
+    """
+    Function gets url and returns a list of urls with all images from url input
+    :param url:
+    :return: list of urls with images
+    """
+
+    r = validate_url(url)
+    parsed_uri = urlparse(url)
+
+    if r:
+        soup = bs.BeautifulSoup(r.text, features='html.parser')
+        url_photos = []
+        for img_tag in soup.find_all('img'):
+            url_photo = img_tag['src']
+            p = urlparse(url_photo)
+            if not p.hostname:
+                url_photo = parsed_uri.hostname + url_photo
+            if not p.scheme:
+                url_photo = parsed_uri.scheme + ':' + url_photo if url_photo.startswith('//') else parsed_uri.scheme + '://' + url_photo
+            if validate_url(url_photo):
+                url_photos.append(url_photo)
+        return url_photos
+
+
+def validate_url(url: Text) -> Union[Response, None]:
+    r = None
+    try:
+        URLValidator(url)
+        r = requests.get(url=url)
+        if not r.status_code == 200:
+            raise requests.HTTPError("Request status code is not equal 200.")
+    except (ValidationError, MissingSchema, requests.HTTPError, ConnectionError) as e:
+        logger.error(e)
+    return r
