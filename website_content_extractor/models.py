99d34a140c2e334cea30596afdc2076af6e640e0
Albert Suraliński <albert.suralinski@gmail.com>
2020-03-21 16:24:05 +0100

    Changed structure of fields with texts and images.

--- website_content_extractor/models.py
+++ website_content_extractor/models.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import Generator, Tuple, List
+from typing import List
 
+from django.contrib.postgres.fields import JSONField
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -19,14 +20,14 @@ class QueueTask(Timestamps, models.Model):
         def value_name_pairs(cls) -> List:
             return list(map(lambda state: (state.value, state.name), cls))
 
-    url = models.URLField(_("URL"), blank=False, max_length=1000, validators=[RegexValidator(
+    url = models.URLField(_("URL"), blank=False, max_length=1000, null=False, validators=[RegexValidator(
         regex='(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})',
         message='Invalid URL'
     )])
-    get_text = models.BooleanField(_("get texts"), default=True)
-    get_image = models.BooleanField(_("get images"), default=True)
+    get_text = models.BooleanField(verbose_name=_("get texts"), default=True)
+    get_image = models.BooleanField(verbose_name=_("get images"), default=True)
     state = models.CharField(
-        _("state"), choices=TaskState.value_name_pairs(), max_length=20, default=TaskState.PENDING.value,
+        verbose_name=_("state"), choices=TaskState.value_name_pairs(), max_length=20, default=TaskState.PENDING.value,
     )
 
     class Meta:
@@ -36,10 +37,11 @@ class QueueTask(Timestamps, models.Model):
 
 
 class WebsiteImage(Timestamps, models.Model):
-    image = models.ImageField(_("image"), upload_to='images/', null=True,)
+    image = models.ImageField(verbose_name=_("image"), upload_to='media/', null=True)
+    image_url = models.URLField(verbose_name=_("image url"), blank=True, max_length=1000)
     task = models.ForeignKey(to=QueueTask, on_delete=models.CASCADE)
 
 
 class WebsiteText(Timestamps, models.Model):
-    text = models.TextField(_("text"), null=True)
+    text = JSONField(verbose_name=_("texts"), default=list, blank=True, null=False)
     task = models.ForeignKey(to=QueueTask, on_delete=models.CASCADE)
