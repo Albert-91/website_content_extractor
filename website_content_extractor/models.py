127d201066a1f0ed13313242d697a98ceedaa67c
Albert Suraliński <albert.suralinski@gmail.com>
2020-03-21 15:52:28 +0100

    Added form adding data to db in task table.

--- website_content_extractor/models.py
+++ website_content_extractor/models.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Generator, Tuple, List
 
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -18,9 +19,12 @@ class QueueTask(Timestamps, models.Model):
         def value_name_pairs(cls) -> List:
             return list(map(lambda state: (state.value, state.name), cls))
 
-    url = models.URLField(_("url"), blank=False, max_length=1000)
-    get_text = models.BooleanField(_("get_text"), default=True)
-    get_image = models.BooleanField(_("get_image"), default=True)
+    url = models.URLField(_("URL"), blank=False, max_length=1000, validators=[RegexValidator(
+        regex='(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})',
+        message='Invalid URL'
+    )])
+    get_text = models.BooleanField(_("get texts"), default=True)
+    get_image = models.BooleanField(_("get images"), default=True)
     state = models.CharField(
         _("state"), choices=TaskState.value_name_pairs(), max_length=20, default=TaskState.PENDING.value,
     )
--- website_content_extractor/views.py
+++ website_content_extractor/views.py
@@ -1,11 +1,36 @@
+import logging
+
+from django.db import DatabaseError, transaction
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import FormView
 
 from website_content_extractor.forms import QueueTaskForm
+from website_content_extractor.models import QueueTask
+
+logger = logging.getLogger(__name__)
 
 
-class QueueTaskView(FormView):
-    template_name = 'queue_task_form.html'
-    form_class = QueueTaskForm
-    success_url = '/'
+class QueueTaskView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = QueueTaskForm()
+        context = {'form': form}
+        return render(request, 'queue_task_form.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = QueueTaskForm(data=request.POST)
+        if form.is_valid():
+            get_text = form.cleaned_data['get_text']
+            get_image = form.cleaned_data['get_image']
+            if get_image or get_text:
+                try:
+                    with transaction.atomic():
+                        QueueTask.objects.create(url=form.cleaned_data['url'],
+                                                 get_text=form.cleaned_data['get_text'],
+                                                 get_image=form.cleaned_data['get_image'])
+                except DatabaseError as e:
+                    logger.error(e)
+                return render(request, 'queue_task_form.html', {'form': QueueTaskForm(), 'status': 'success'})
+            else:
+                return render(request, 'queue_task_form.html', {'form': form, 'status': 'warning'})
+        return render(request, 'queue_task_form.html', {'form': form})
