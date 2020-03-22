1b8bc8f9df08844ace20bcbf9cc50291ec48bb41
Albert Suraliński <albert.suralinski@gmail.com>
2020-03-22 15:11:06 +0100

    Changed names of class and methods.

--- website_content_extractor/tests.py
+++ website_content_extractor/tests.py
@@ -1,37 +1,37 @@
 from django.test import TestCase
 
-from website_content_extractor.utils import get_url_images_from_html, get_text_from_html
+from website_content_extractor.utils import get_url_images_from_html, get_text_from_html, validate_url
 
 
-class GetUrlPhotosFromHtml(TestCase):
+class GetUrlImagesFromHtml(TestCase):
 
-    def test__photo_url_extractor__should__return__None_if_url_is_not_string(self):
+    def test__image_url_extractor__should__return__None_if_url_is_not_string(self):
         self.assertEqual(get_url_images_from_html(2), None)
 
-    def test__photo_url_extractor__should__return__None_if_url_has_no_scheme(self):
+    def test__image_url_extractor__should__return__None_if_url_has_no_scheme(self):
         self.assertEqual(get_url_images_from_html('www.google.pl'), None)
 
-    def test__photo_url_extractor__should__return__None_if_scheme_is_invalid(self):
+    def test__image_url_extractor__should__return__None_if_scheme_is_invalid(self):
         self.assertEqual(get_url_images_from_html('htts://www.google.pl'), None)
 
-    def test__photo_url_extractor__should__return__None__if_status_code_is_404(self):
+    def test__image_url_extractor__should__return__None__if_status_code_is_404(self):
         url = "https://github.com/Albert-91/semantive_scrapping_text_and_images_from_url"
         self.assertEqual(get_url_images_from_html(url), None)
 
-    def test__photo_url_extractor__should__return__None_if_domain_does_not_exist(self):
+    def test__image_url_extractor__should__return__None_if_domain_does_not_exist(self):
         self.assertEqual(get_url_images_from_html('http://www.onetwothree.pl'), None)
 
-    def test__photo_url_extractor__should__return__None_input_is_not_correct_url(self):
+    def test__image_url_extractor__should__return__None_input_is_not_correct_url(self):
         self.assertEqual(get_url_images_from_html('http://www.onetwothreepl'), None)
 
-    def test__photo_url_extractor__should__return__not_empty_list(self):
+    def test__image_url_extractor__should__return__not_empty_list(self):
         len_of_list = len(get_url_images_from_html('http://www.google.pl'))
         self.assertGreater(len_of_list, 0)
 
-    def test__photo_url_extractor__should__return__list_with_correct_urls(self):
-        photo_urls = get_url_images_from_html('https://en.wikipedia.org/wiki/Hulk')
-        for url in photo_urls:
-            self.assertNotEqual(get_url_images_from_html(url), None)
+    def test__image_url_extractor__should__return__list_with_correct_urls(self):
+        image_urls = get_url_images_from_html('https://en.wikipedia.org/wiki/Hulk')
+        for url in image_urls:
+            self.assertNotEqual(validate_url(url), None)
 
 
 class GetTextFromHtml(TestCase):
